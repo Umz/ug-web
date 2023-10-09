b120ae97d92a07897b6cdade7878cebfb3a0c6cd
Umz <umz@umzgames.com>
2023-10-09 16:53:10 +0100

    Created the basic structure to be used for T&C + Privacy pages-

---
+++ pages/legal/kae.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <div class="contained-content">
+
+        <div id="legal-heading">
+            <h1>{{ gameName }}</h1>
+        </div>
+
+        <section id="terms-of-use">
+            <h2>Terms of Use</h2>
+            <div class="legal-content">
+                {{ gameTerms }}
+            </div>
+        </section>
+
+        <div class="divider"></div>
+
+        <section id="privacy">
+            <h2>Privacy Policy</h2>
+            <div class="legal-content">
+                {{ gamePrivacy }}
+            </div>
+        </section>
+
+    </div>
+
+</template>
+
+<style lang="css">
+  @import url('~/assets/css/legal.css');
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      gameName: 'Killer Ant Empire / KAE',
+      gameTerms: 'These are the terms for KAE',
+      gamePrivacy: 'This is the privacy for KAE'
+    }
+  }
+}
+</script>
