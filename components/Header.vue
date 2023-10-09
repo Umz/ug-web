72d54e300bc45920a989f2482a477db97e91a616
Umz <umz@umzgames.com>
2023-10-09 15:42:14 +0100

    Updated the structure of the header-

--- components/Header.vue
+++ components/Header.vue
@@ -3,6 +3,10 @@
         <div id="header-logo">
             <img id="header-image" src="~/assets/images/umzgames512w.png" alt="Umz Games" />
         </div>
+        <div id="header-navigation">
+            <p>About</p>
+            <p>Contact</p>
+        </div>
     </header>
 </template>
 
