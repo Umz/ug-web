028f6e325deaf7578854b582a366f956d4da746a
Umz <umz@umzgames.com>
2023-10-21 17:10:28 +0100

    Index top section styled-

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>
-        This is content for the home page
-    </div>
+    
+    <section>
+        <div class="top-flex">
+            <h2>We are<br />Umz Games</h2>
+            <div class="top-image">
+                <img src="~/assets/images/slimes-throne.png" alt="Slime-S Throne" />
+            </div>
+        </div>
+    </section>
+
 </template>
+
+<style lang="css" scoped>
+  @import url('~/assets/css/home.css');
+</style>
+
+<script>
+</script>
