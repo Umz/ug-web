e7f46f1354b4f073e02ef1bb13b7cde1d98a0205
Umz <umz@umzgames.com>
2023-10-21 17:08:41 +0100

    Updating the CSS header and footer, fonts and styles

--- app.vue
+++ app.vue
@@ -10,5 +10,6 @@
 </template>
 
 <style lang="css">
+  @import url('https://fonts.googleapis.com/css2?family=Bungee&family=Montserrat&display=swap');
   @import url('~/assets/css/styles.css');
 </style>
