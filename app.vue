c7243a50fc989fada2c380598a0c2f266be8425d
Umz <umz@umzgames.com>
2023-10-21 17:50:36 +0100

    Updated style for the Header

--- app.vue
+++ app.vue
@@ -10,6 +10,6 @@
 </template>
 
 <style lang="css">
-  @import url('https://fonts.googleapis.com/css2?family=Bungee&family=Montserrat&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Bungee&family=Montserrat:wght@400;600&display=swap');
   @import url('~/assets/css/styles.css');
 </style>
--- components/Header.vue
+++ components/Header.vue
@@ -3,11 +3,11 @@
         <div id="header-logo">
             <img id="header-image" src="~/assets/images/umzgames512w.png" alt="Umz Games" />
         </div>
-        <div id="header-navigation">
-            <p>About</p>
-            <p>Contact</p>
-        </div>
     </header>
+    <div id="header-navigation">
+        <p>About</p>
+        <p>Contact</p>
+    </div>
 </template>
 
 <style lang="css">
